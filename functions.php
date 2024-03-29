955eadcd7d2a105b82ffce4ee0aa6e34067e723b
David Alejandro Gómez <david@acapillatech.com.ar>
2024-03-29 17:32:56 -0300

    Script para capacitaciones en front-page

--- functions.php
+++ functions.php
@@ -19,6 +19,24 @@ function bootscore_child_enqueue_styles()
     wp_enqueue_style('splide-css', get_stylesheet_directory_uri() . '/css/splide-default.min.css', array(), null);
     wp_enqueue_script('capacitaciones-front', get_stylesheet_directory_uri() . '/js/capacitaciones-front.js', false, array('splide-js'), true);
   endif;
+
+  if(is_page('test')):
+    wp_enqueue_script('test-js', get_stylesheet_directory_uri() . '/js/test.js', false, '', true);
+  endif;
+
+  function add_module_attribute($tag, $handle, $src) {
+    // if not your script, do nothing and return original $tag
+    if ( 'capacitaciones-front' !== $handle ) {
+        return $tag;
+    }
+    
+    // change the script tag by adding type="module" and return it.
+    $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
+    
+    return $tag;
+  }
+  
+  add_filter('script_loader_tag', 'add_module_attribute', 10, 3);
 }
 
 require_once(__DIR__ . '/inc/shortcodes.php');
