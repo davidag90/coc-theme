673ab7acec21a1e668285f27d80b424f62c811cd
David Alejandro Gómez <david@acapillatech.com.ar>
2024-03-29 17:33:37 -0300

    Rework de componente para capacitaciones front-end

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -9,72 +9,36 @@ function capacitaciones_front()
       'taxonomy' => 'especialidad'
    ));
 
-   if ($especialidades) {
-      $output .= '<div id="capacitaciones-front" class="py-5">';
+   if($especialidades):
+      $output .= '<div id="capacitaciones-front" class="py-5 px-4">';
          $output .= '<h2 class="mb-4 text-center display-4 fw-bold">Capacitaciones por Especialidad</h2>';
 
-         $output .= '<div class="d-flex flex-row flex-wrap justify-content-center mb-4" id="filtros-espec">';
+         $output .= '<div class="d-block d-md-none mb-4" id="filtros-espec-mobile">';
+            $output .= '<select class="form-select">';
+               $output .= '<option value="todos" selected>Todos</option>';
+               foreach ($especialidades as $especialidad) {
+                  $output .= '<option value="' . esc_attr($especialidad->slug) . '">' . esc_html($especialidad->name) . '</option>';
+               }
+            $output .='</select>'; // .form-select
+         $output .= '</div>';// #filtros-espec-mobile
+         $output .= '<div class="d-none d-md-flex flex-row flex-wrap justify-content-center mb-4" id="filtros-espec-desk">';
             $output .= '<button type="button" class="btn btn-sm btn-todos text-nowrap filtro-espec" coc-especialidad="todos">Todos</button>';
 
             foreach ($especialidades as $especialidad) {
                $output .= '<button type="button" class="btn btn-sm btn-outline-dark text-nowrap filtro-espec" coc-especialidad="' . esc_attr($especialidad->slug) . '">' . esc_html($especialidad->name) . '</button>';
             }
-         $output .= '</div>'; // #filtros-espec
+         $output .= '</div>'; // #filtros-espec-desk
 
-         $args = array(
-            'post_type' => 'capacitacion'
-         );
+         $output .= '<div id="app-root" class="splide mb-5">';
+            $output .= '<div class="splide__track">';
+               $output .= '<ul class="splide__list">';
+               $output .= '</ul>'; // .splide__list
+            $output .= '</div>'; // .splide__track
+         $output .= '</div>'; // .splide
 
-         $query = new WP_Query($args);
-
-         if ($query->have_posts()) {
-            $output .= '<div id="capacitaciones" class="splide mb-4">';
-               $output .= '<div class="splide__track">';
-                  $output .= '<ul class="splide__list">';
-                     while ($query->have_posts()) {
-                        $query->the_post();
-
-                        $img_top = get_the_post_thumbnail_url();
-                        $titulo = get_the_title();
-                        $inicio = get_field('fecha_inicio');
-                        $link = get_the_permalink();
-                        
-                        $terms = get_the_terms(get_the_ID(), 'especialidad');
-                        $especialidad_slug = '';
-                        $especialidad_nombre = '';
-                        if ($terms && !is_wp_error($terms)) {
-                           $especialidad_slug = $terms[0]->slug;
-                           $especialidad_nombre = $terms[0]->name;
-                        }
-
-                        $tipo_capacitacion = get_field('tipo_capacitacion');
-
-                        $output .= '<li class="splide__slide">';
-                           $output .= '<div class="card border-' . esc_attr($especialidad_slug) . ' h-100 capacitacion" coc-especialidad="' . esc_attr($especialidad_slug) . '">';
-                              $output .= '<img class="card-img-top" src="' . esc_url($img_top) . '" alt="' . esc_attr($titulo) . '" />';
-                              $output .= '<div class="card-body d-flex flex-column">';
-                                 $output .= '<p class="mb-2"><small>' . $tipo_capacitacion . ' en ' . $especialidad_nombre . '</small></p>';
-                                 $output .= '<h4 class="card-title h6">' . esc_html($titulo) . '</h4>';
-                                 $output .= '<p class="card-text">' . esc_html($inicio) . '</p>';
-                                 $output .= '<a href="' . esc_url($link) . '" class="btn btn-sm btn-' . esc_attr($especialidad_slug) . ' mt-auto ms-auto me-0 text-nowrap">Más información</a>';
-                              $output .= '</div>'; // .card-body
-                           $output .= '</div>'; // .card
-                        $output .= '</li>'; // .splide__slide
-                     }
-                  $output .= '</ul>'; // .splide__list
-               $output .= '</div>'; // .splide__track
-            $output .= '</div>'; // #capacitaciones
-         } else {
-            $output .= '<p>No hay capacitaciones disponibles.</p>';
-         }
-      
          $output .= '<div id="acceso-capacitaciones-full" class="d-flex justify-content-center"><a class="btn btn-secondary btn-lg" href="' . home_url() . '/capacitacion/">Ver nuestra agenda completa</a></div>';
       $output .= '</div>'; // #capacitaciones-front
-
-      wp_reset_postdata();
-   } else {
-      $output .= '<p>No hay especialidades disponibles.</p>';
-   }
+   endif;
 
    echo $output;
    return ob_get_clean();
--- js/capacitaciones-front.js
+++ js/capacitaciones-front.js
@@ -1,63 +1,179 @@
-jQuery(function($) {
-   $(document).ready(function(){
-      const capacitaciones = $('.capacitacion');
+const appRoot = document.querySelector('.splide > .splide__track > .splide__list');
+const APIURL = 'http://coc-dev.local/wp-json/wp/v2/capacitacion?_embed';
 
-      $('.filtro-espec').click( function() {
-         let especialidad = $(this).attr('coc-especialidad');
-         
-         $('.filtro-espec').each(function() {
-            $(this).removeClass(`btn-${$(this).attr('coc-especialidad')}`);
-            $(this).addClass(`btn-outline-dark`);
-         });
-         
-         $(this).addClass(`btn-${especialidad}`);
-         
-         if(especialidad === 'todos') {
-            setTimeout( () => {
-               $('.capacitacion').show();
-               $('.capacitacion').css('opacity', '1');
-            }, 250);
-         } else {
-            $('.capacitacion').css('opacity', 0);
-            setTimeout( () => {
-               $('.capacitacion').hide();
+async function fetchData(url) {
+   const response = await fetch(url);
+   
+   return await response.json();
+}
 
-            }, 250);
+async function setData(url) {
+   const data = await fetchData(url);
+   const posts = data.map((element) => {
+     return {
+       tipoCapacitacion: element.acf.tipo_capacitacion,
+       especialidadSlug: element._embedded['wp:term'][0][0].slug,
+       especialidadNombre: element._embedded['wp:term'][0][0].name,
+       titulo: element.title.rendered,
+       fechaInicio: element.acf.fecha_inicio,
+       thumbnail: element._embedded['wp:featuredmedia'][0].link,
+       link: element.link
+     }
+   });
+  
+  return posts;
+}
+
+const capacitaciones = await setData(APIURL);
+
+const fillCapacitaciones = (jsonCapacitaciones, especialidad = 'todos') => {
+   jsonCapacitaciones.forEach((element) => {
+      if(especialidad === 'todos') {
+         let card = document.createElement('div');
+         card.classList.add('card', 'capacitacion', `border-${element.especialidadSlug}`, 'h-100');
+         card.setAttribute('coc-especialidad', element.especialidadSlug);
+      
+         let cardImgTop = document.createElement('img');
+         cardImgTop.setAttribute('src', element.thumbnail);
+         cardImgTop.classList.add('card-img-top');
+         
+         let cardBody = document.createElement('div');
+         cardBody.classList.add('card-body', 'd-flex', 'flex-column');
+         
+         let cardTitle = document.createElement('h3');
+         cardTitle.classList.add('card-title', 'h5');
+         cardTitle.innerHTML = element.titulo;
+      
+         let cardSubtitle = document.createElement('span');
+         cardSubtitle.classList.add('d-block', 'text-secondary');
+         
+         let cardSubtitleInner = document.createElement('small');
+         cardSubtitleInner.innerHTML = `${element.tipoCapacitacion} en ${element.especialidadNombre}`;
+         
+         cardSubtitle.append(cardSubtitleInner);
+      
+         let cardText = document.createElement('p');
+         cardText.classList.add('card-text');
+         cardText.innerHTML = element.fechaInicio;
+      
+         let callToAction = document.createElement('a');
+         callToAction.setAttribute('href', element.link);
+         callToAction.classList.add('btn', 'btn-sm', `btn-${element.especialidadSlug}`, 'ms-auto', 'mt-auto');
+         callToAction.innerText = 'Más información';
+      
+         cardBody.append(cardSubtitle, cardTitle, cardText, callToAction);
+         
+         card.append(cardImgTop, cardBody);
+      
+         let slide = document.createElement('li');
+         slide.classList.add('splide__slide');
+         slide.append(card);
+         
+         appRoot.append(slide);
+      } else {
+         if(especialidad === element.especialidadSlug) {
+            let card = document.createElement('div');
+            card.classList.add('card', 'capacitacion', `border-${element.especialidadSlug}`, 'h-100');
+            card.setAttribute('coc-especialidad', element.especialidadSlug);
+         
+            let cardImgTop = document.createElement('img');
+            cardImgTop.setAttribute('src', element.thumbnail);
+            cardImgTop.classList.add('card-img-top');
             
-            setTimeout( () => {
-               $(`.capacitacion[coc-especialidad="${especialidad}"]`).show();
-               $(`.capacitacion[coc-especialidad="${especialidad}"]`).css('opacity', '1');
-            }, 250);
+            let cardBody = document.createElement('div');
+            cardBody.classList.add('card-body', 'd-flex', 'flex-column');
+            
+            let cardTitle = document.createElement('h3');
+            cardTitle.classList.add('card-title', 'h5');
+            cardTitle.innerHTML = element.titulo;
+         
+            let cardSubtitle = document.createElement('span');
+            cardSubtitle.classList.add('d-block', 'text-secondary');
+            
+            let cardSubtitleInner = document.createElement('small');
+            cardSubtitleInner.innerHTML = `${element.tipoCapacitacion} en ${element.especialidadNombre}`;
+            
+            cardSubtitle.append(cardSubtitleInner);
+         
+            let cardText = document.createElement('p');
+            cardText.classList.add('card-text');
+            cardText.innerHTML = element.fechaInicio;
+         
+            let callToAction = document.createElement('a');
+            callToAction.setAttribute('href', element.link);
+            callToAction.classList.add('btn', 'btn-sm', `btn-${element.especialidadSlug}`, 'ms-auto', 'mt-auto');
+            callToAction.innerText = 'Más información';
+         
+            cardBody.append(cardSubtitle, cardTitle, cardText, callToAction);
+            
+            card.append(cardImgTop, cardBody);
+         
+            let slide = document.createElement('li');
+            slide.classList.add('splide__slide');
+            slide.append(card);
+            
+            appRoot.append(slide);
          }
-      });
+      }
+   });
 
-      const splideCapacitaciones = new Splide( '.splide', {
-         type: 'slide',
-         mediaQuery: 'min',
-         gap: '1rem',
-         padding: '1rem',
-         pagination: false,
-         breakpoints: {
-            0: {
-               perPage: 1,
-               perMove: 1
-            },
-            576: {
-               perPage: 2,
-               perMove: 1
-            },
-            768: {
-               perPage: 3,
-               perMove: 1
-            },
-            1200: {
-               perPage: 4,
-               perMove: 1
-            }
+   const splideCapacitaciones = new Splide( '.splide', {
+      type: 'slide',
+      mediaQuery: 'min',
+      gap: '1rem',
+      padding: '1rem',
+      pagination: false,
+      breakpoints: {
+         0: {
+            perPage: 1,
+            perMove: 1
+         },
+         576: {
+            perPage: 2,
+            perMove: 1
+         },
+         768: {
+            perPage: 3,
+            perMove: 1
+         },
+         1200: {
+            perPage: 4,
+            perMove: 1
          }
-      });
+      }
+   });
+   
+   splideCapacitaciones.mount();
+}
 
-      splideCapacitaciones.mount();
+document.addEventListener('DOMContentLoaded', fillCapacitaciones(capacitaciones));
+
+const filtros = document.querySelectorAll('.filtro-espec');
+
+filtros.forEach( (filtro) => {
+   let especialidad = filtro.getAttribute('coc-especialidad');
+   
+   filtro.addEventListener('click', (event) => {
+      appRoot.innerHTML = '';
+      fillCapacitaciones(capacitaciones, especialidad);
+      
+      filtros.forEach(elem => {
+         let especialidadIn = elem.getAttribute('coc-especialidad');
+         elem.classList.remove(`btn-${especialidadIn}`);
+         elem.classList.add(`btn-outline-dark`);
+      });
+      
+      event.target.classList.remove(`btn-outline-dark`);
+      event.target.classList.add(`btn-${especialidad}`);
    });
 });
 
+const filtrosMobile = document.querySelector('#filtros-espec-mobile > select');
+
+filtrosMobile.addEventListener('change', (event) => {
+   let especialidad = event.target.value;
+
+   appRoot.innerHTML = '';
+   
+   fillCapacitaciones(capacitaciones, especialidad);
+});
