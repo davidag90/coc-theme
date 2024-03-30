3d5dc974160a3396940c5a252abda3033c85e01e
David Alejandro Gómez <david@acapillatech.com.ar>
2024-03-30 07:53:31 -0300

    Reorganizacion de layout en paginas internas

--- page.php
+++ page.php
@@ -18,15 +18,9 @@ get_header();
 
   <div id="content" class="site-content <?= bootscore_container_class(); ?> py-5 mt-5">
     <div id="primary" class="content-area">
-
-      <!-- Hook to add something nice -->
-      <?php bs_after_primary(); ?>
-
       <div class="row">
-        <div class="<?= bootscore_main_col_class(); ?>">
-
+        <div class="col-12 col-lg-8">
           <main id="main" class="site-main">
-
             <header class="entry-header">
               <?php the_post(); ?>
               <h1><?php the_title(); ?></h1>
@@ -36,17 +30,11 @@ get_header();
             <div class="entry-content">
               <?php the_content(); ?>
             </div>
-
-            <footer class="entry-footer">
-              <?php comments_template(); ?>
-            </footer>
-
           </main>
-
         </div>
-        <?php get_sidebar(); ?>
+        
+        <?php get_sidebar('page'); ?>
       </div>
-
     </div>
   </div>
 
