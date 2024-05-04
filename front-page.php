af3e717888aa45dc9644776206dc5ca87b8fe5b7
David Alejandro Gómez <david@acapillatech.com.ar>
2024-05-04 09:32:48 -0300

    Controles de slideshow

--- front-page.php
+++ front-page.php
@@ -1,65 +1,77 @@
   <?php
 
-  /**
-   * Front-Page Template
-   *
-   * @package Bootscore
-   */
+	/**
+	 * Front-Page Template
+	 *
+	 * @package Bootscore
+	 */
 
-  get_header();
-  ?>
+	get_header();
+	?>
 
   <div id="content" class="site-content">
-    <div id="hero-unit">
-      <?php $args = array(
-        'post_type' => 'home_slides',
-        'posts_per_page' => 3
-      );
+  	<div id="hero-unit">
+  		<?php $args = array(
+				'post_type' => 'home_slides',
+				'posts_per_page' => 3
+			);
 
-      $slide_query = new WP_Query($args);
+			$slide_query = new WP_Query($args);
 
-      if ($slide_query->have_posts()) : ?>
-        <div id="front-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-touch="true">
-          <div class="carousel-inner">
-            <?php $c = 0; ?>
-            <?php while ($slide_query->have_posts()) : $slide_query->the_post(); ?>
-              <div class="carousel-item <?php if ($c == 0) : ?>active<?php endif; ?>">
-                <?php $link = get_field('content_link');
-                
-                if ($link) : // Verifica si el slide tiene un link asociado ?>
-                <a class="d-block w-100" href="<?php echo $link['url']; ?>">
-                <?php endif; ?>
-                  <picture>
-                    <?php
-                    $slide_desktop = get_field('slide_desktop');
-                    $slide_mobile = get_field('slide_mobile');
-                    ?>
+			if ($slide_query->have_posts()) : ?>
+  			<div id="front-carousel" class="carousel slide" data-bs-ride="carousel" data-bs-touch="true">
+  				<div class="carousel-inner">
+  					<?php $c = 0; ?>
+  					<?php while ($slide_query->have_posts()) : $slide_query->the_post(); ?>
+  						<div class="carousel-item <?php if ($c == 0) : ?>active<?php endif; ?> position-relative">
+  							<?php $link = get_field('content_link');
 
-                    <source srcset="<?php echo esc_url($slide_mobile['url']); ?>" class="d-block w-100" media="(max-width:768px)">
-                    <img src="<?php echo esc_url($slide_desktop['url']); ?>" class="d-block w-100">
-                  </picture>
-                <?php if ($link) : // Verificación para cerrar el <a> en caso de haber link asociado ?>
-                </a>
-                <?php endif; ?>
-              </div><!-- .carousel-item -->
-              
-              <?php $c++; ?>
-            <?php endwhile; ?>
-          </div><!-- .carousel-inner -->
-        </div><!-- #front-carousel -->
-        <?php wp_reset_postdata(); ?>
-      <?php endif; ?>
-    </div>
-    <div id="primary" class="content-area">
-      <main id="main" class="site-main">
-        <div class="entry-content">
-          <div class="container">
-            <?php the_post(); ?>
-            <?php the_content(); ?>
-          </div><!-- .container -->
-        </div><!-- .entry-content -->
-      </main><!-- .site-main -->
-    </div><!-- #primary -->
+								if ($link) : // Verifica si el slide tiene un link asociado 
+								?>
+  								<a class="d-block w-100" href="<?php echo $link['url']; ?>">
+  								<?php endif; ?>
+  								<picture>
+  									<?php
+										$slide_desktop = get_field('slide_desktop');
+										$slide_mobile = get_field('slide_mobile');
+										?>
+
+  									<source srcset="<?php echo esc_url($slide_mobile['url']); ?>" class="d-block w-100" media="(max-width:768px)">
+  									<img src="<?php echo esc_url($slide_desktop['url']); ?>" class="d-block w-100">
+  								</picture>
+  								<?php if ($link) : // Verificación para cerrar el <a> en caso de haber link asociado 
+									?>
+  								</a>
+  							<?php endif; ?>
+  						</div><!-- .carousel-item -->
+  						<?php $c++; ?>
+  					<?php endwhile; ?>
+  				</div><!-- .carousel-inner -->
+				
+				<button class="carousel-control-prev" type="button" data-bs-target="#front-carousel" data-bs-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Anterior</span>
+				</button>
+				
+				<button class="carousel-control-next" type="button" data-bs-target="#front-carousel" data-bs-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Siguiente</span>
+				</button>
+  			</div><!-- #front-carousel -->
+			
+  			<?php wp_reset_postdata(); ?>
+  		<?php endif; ?>
+  	</div>
+  	<div id="primary" class="content-area">
+  		<main id="main" class="site-main">
+  			<div class="entry-content">
+  				<div class="container">
+  					<?php the_post(); ?>
+  					<?php the_content(); ?>
+  				</div><!-- .container -->
+  			</div><!-- .entry-content -->
+  		</main><!-- .site-main -->
+  	</div><!-- #primary -->
   </div><!-- #content -->
 
   <?php get_footer();
