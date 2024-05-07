874a24d732a4f29c4aeaac6b5edb3e4e4dc520fb
David Alejandro Gómez <david@acapillatech.com.ar>
2024-05-06 21:10:41 -0300

    Adición de botón homepage para menú principal

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 jQuery(function ($) {
     let homeItem = `<li class="menu-item menu-item-home nav-item">
                         <a href="${envParams.SITE_URL}" class="nav-link">
-                            <i class="fa-solid fa-house-chimney fa-2x"></i> <span class="d-none">Inicio</span>
+                            <i class="fa-solid fa-house-chimney"></i> <span class="d-none">Inicio</span>
                         </a>
                     </li>`;
     
