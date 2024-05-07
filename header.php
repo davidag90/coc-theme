006d7094055f99bb6ee2db16c9324879579d3753
David Alejandro Gómez <david@acapillatech.com.ar>
2024-05-07 16:16:39 -0300

    Cambio de Bootstrap Icons por FontAwesome

--- header.php
+++ header.php
@@ -52,8 +52,8 @@
 							</div>
 							<div class="offcanvas-body bg-primary flex-lg-column">
 								<div id="plus-nav" class="d-flex flex-column flex-lg-row align-items-start align-items-lg-center justify-content-lg-end mb-2 mb-lg-0">
-									<a href="http://certificaciones.coc-cordoba.org.ar/" class="btn btn-secondary btn-sm" target="_blank"><i class="fa-solid fa-file-lines"></i> Certificaciones</a>
-									<a href="http://www.coc-autogestion.com.ar/" class="btn btn-secondary btn-sm" target="_blank"><i class="fa-solid fa-user"></i> Autogestión Socios</a>
+									<a href="http://certificaciones.coc-cordoba.org.ar/" class="btn btn-secondary btn-sm" target="_blank"><i class="fa-solid fa-file-lines me-1"></i> Certificaciones</a>
+									<a href="http://www.coc-autogestion.com.ar/" class="btn btn-secondary btn-sm" target="_blank"><i class="fa-solid fa-user me-1"></i> Autogestión Socios</a>
 									
 									<div id="nav-search-wrap" class="input-group input-group-sm">
 										<input type="text" class="form-control bg-secondary text-light" placeholder="Buscar en el sitio..." aria-label="Buscar en el sitio" aria-describedby="button-search" style="--bs-border-opacity: .5;">
