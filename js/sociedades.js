3125dfbe8746402ad0a09b788d91824e446aef68
David Alejandro Gómez <david@acapillatech.com.ar>
2024-05-20 15:56:44 -0300

    Adición de ordenamiento alfabetico por titulo

--- js/sociedades.js
+++ js/sociedades.js
@@ -73,6 +73,16 @@ function createModals(objSociedad) {
 }
 
 function fillSociedades(jsonSociedades) {
+   jsonSociedades.sort((a,b) => {
+      let x = a.title.toLowerCase();
+      let y = b.title.toLowerCase();
+      
+      if (x < y) { return -1 }
+      if (x > y) { return 1 }
+      
+      return 0;
+   });
+
    jsonSociedades.forEach((element) => {
       createItem(element);
       createModals(element);
