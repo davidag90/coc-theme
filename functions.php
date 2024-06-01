1ed4a58836e476cfc0c7c9d38eb11121d614c1b4
David Alejandro Gómez <david@acapillatech.com.ar>
2024-06-01 15:23:35 -0300

    Personalizacion de email pedido completado

--- functions.php
+++ functions.php
@@ -122,14 +122,14 @@ add_filter( 'woocommerce_add_to_cart_validation', 'single_item_cart', 20, 3 );
 /**
  * Add a custom field (in an order) to the emails
  */
-add_filter( 'woocommerce_email_order_meta_fields', 'custom_woocommerce_email_order_meta_fields', 10, 3 );
+add_filter( 'woocommerce_email_order_meta_fields', 'custom_woocommerce_email_order_meta_fields', 20, 3 );
 
 function custom_woocommerce_email_order_meta_fields( $fields, $sent_to_admin, $order ) {
     $fields['billing_wooccm12'] = array(
         'label' => 'DNI',
         'value' => get_post_meta( $order->id, 'billing_wooccm12', true ),
     );
-    
+
     return $fields;
 }
 
