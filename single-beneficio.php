792d2f6e36ddd4dfc4cb5cbcf7f3b7260c3efe4f
David Alejandro Gómez <david@acapillatech.com.ar>
2024-06-27 12:27:06 -0300

    Ajuste layout template individual para Beneficios

--- single-beneficio.php
+++ single-beneficio.php
@@ -9,48 +9,30 @@ get_header();
 
   <div id="content" class="site-content <?= bootscore_container_class(); ?> py-5 mt-4">
     <div id="primary" class="content-area">
-
-      <!-- Hook to add something nice -->
-      <?php bs_after_primary(); ?>
-
-      <?php the_breadcrumb(); ?>
-
       <div class="row">
         <div class="<?= bootscore_main_col_class(); ?>">
-
           <main id="main" class="site-main">
-
             <header class="entry-header">
               <?php the_post(); ?>
               <h1><?php the_title(); ?></h1>
             </header>
 
             <div class="entry-content">
-              <?php bootscore_post_thumbnail(); ?>
-              <?= get_field('detalles'); ?>
+              <div class="row">
+                <div class="col-12 col-md-4">
+                  <a href="<?= get_the_post_thumbnail_url(); ?>" target="_blank"><?php the_post_thumbnail(); ?></a>
+                </div>
+                <div class="col-12 col-md-8">
+                  <?= get_field('detalles'); ?>
+                </div>
+              </div>
             </div>
-
-            <footer class="entry-footer clear-both">
-              <!-- Related posts using bS Swiper plugin -->
-              <?php if (function_exists('bootscore_related_posts')) bootscore_related_posts(); ?>
-              
-              <nav aria-label="bS page navigation">
-                <ul class="pagination justify-content-center">
-                  <li class="page-item">
-                    <?php previous_post_link('%link'); ?>
-                  </li>
-                  <li class="page-item">
-                    <?php next_post_link('%link'); ?>
-                  </li>
-                </ul>
-              </nav>
-            </footer>
           </main>
         </div>
-        <?php get_sidebar(); ?>
-      </div>
-    </div>
-  </div>
 
-<?php
-get_footer();
+        <?php get_sidebar(); ?>
+      </div><!-- .row -->
+    </div><!-- #primary -->
+  </div><!-- #content -->
+
+<?php get_footer();
