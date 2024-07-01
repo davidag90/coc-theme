f65aaa29e0e9abc382f7195a429400719bd3e767
David Alejandro Gómez <david@acapillatech.com.ar>
2024-07-01 16:18:59 -0300

    Correccion en escucha de evento ready

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,9 @@
 jQuery(function ($) {
-    let homeItem = `<li class="menu-item menu-item-home nav-item">
-                        <a href="${envParams.SITE_URL}" class="nav-link"><i class="bi bi-house-fill"></i> Inicio</a>
-                    </li>`;
-    
-    $('#bootscore-navbar').prepend(homeItem);
+    $(document).ready(function() {
+        let homeItem = `<li class="menu-item menu-item-home nav-item">
+                            <a href="${envParams.SITE_URL}" class="nav-link"><i class="bi bi-house-fill"></i> Inicio</a>
+                        </li>`;
+        
+        $('#bootscore-navbar').prepend(homeItem);
+    });
 }); // jQuery End
