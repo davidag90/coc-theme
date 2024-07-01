833ad8a17276947780ec26d5ffc05e83adc1ec15
David Alejandro Gómez <david@acapillatech.com.ar>
2024-07-01 16:22:18 -0300

    Cambio a Fontawesome en set de iconos

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,9 @@
 jQuery(function ($) {
     $(document).ready(function() {
         let homeItem = `<li class="menu-item menu-item-home nav-item">
-                            <a href="${envParams.SITE_URL}" class="nav-link"><i class="bi bi-house-fill"></i> Inicio</a>
+                            <a href="${envParams.SITE_URL}" class="nav-link"><i class="fa-solid fa-house-chimney"></i> Inicio</a>
                         </li>`;
         
         $('#bootscore-navbar').prepend(homeItem);
     });
-}); // jQuery End
+});
