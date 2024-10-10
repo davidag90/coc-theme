75da18ce552676475d0c8d91c50f46a840988206
David Gomez <info@dagdev.com.ar>
2024-10-10 14:18:49 -0300

    Fix: spacing corrections on layout

--- single-pgc_simply_gallery.php
+++ single-pgc_simply_gallery.php
@@ -9,14 +9,14 @@
 get_header();
 ?>
 
-<div id="content" class="site-content <?= bootscore_container_class(); ?> py-5 mt-4">
+<div id="content" class="site-content <?= bootscore_container_class(); ?>">
   <div id="primary" class="content-area">
 
     <!-- Hook to add something nice -->
     <div class="row">
       <div class="col-12">
         <main id="main" class="site-main">
-          <header class="entry-header">
+          <header class="entry-header d-none">
             <?php the_post(); ?>
             <h1><?php the_title(); ?></h1>
           </header>
