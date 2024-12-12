1a486e0671184e6e5c4a7da7dd737ed3823bf278
davidag90 <info@dagdev.com.ar>
2024-12-12 16:11:20 -0300

    Fix: excesive padding correction

--- woocommerce.php
+++ woocommerce.php
@@ -16,23 +16,23 @@
 get_header();
 ?>
 
-  <div id="content" class="site-content <?= bootscore_container_class(); ?> py-5 mt-4">
-    <div id="primary" class="content-area">
+<div id="content" class="site-content <?= bootscore_container_class(); ?> pb-5 mt-4">
+  <div id="primary" class="content-area">
 
-      <!-- Hook to add something nice -->
-      <?php bs_after_primary(); ?>
+    <!-- Hook to add something nice -->
+    <?php bs_after_primary(); ?>
 
-      <main id="main" class="site-main">
+    <main id="main" class="site-main">
 
-        <!-- Breadcrumb -->
-        <?php woocommerce_breadcrumb(); ?>
-        <div class="row">
-          <div class="col-12">
-            <?php woocommerce_content(); ?>
-          </div>
+      <!-- Breadcrumb -->
+      <?php woocommerce_breadcrumb(); ?>
+      <div class="row">
+        <div class="col-12">
+          <?php woocommerce_content(); ?>
         </div>
-      </main><!-- #main -->
-    </div><!-- #primary -->
-  </div><!-- #content -->
+      </div>
+    </main><!-- #main -->
+  </div><!-- #primary -->
+</div><!-- #content -->
 <?php
 get_footer();
